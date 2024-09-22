652ecb78e1444466066af6e4087ac33a2fba6cd9
giuse-pio <giupeppepio7@gmail.com>
2024-09-22 16:26:05 +0200

    Initial commit

---
+++ 2.1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    float a;
+    float b;
+    float media;
+    printf("inserisci un numbero: ");
+    scanf("%f", &a);
+    printf("inserisci un numero: ");
+    scanf("%f", &b);
+    media = (a + b)/2;
+    printf("media = %f", media);
+}
